9c78566c02ee80fa0fb14de2fe16cf0a42c417d4
Renan Lopes <renanlopescoder@gmail.com>
2019-12-09 21:37:17 +0100

    Initial commit

---
+++ components/CounterButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CounterButton = React.memo(({ onClick, buttonText }) => {
+    console.log("render");
+    return (
+        <>
+            <button
+                style={{ margin: "10px", padding: "10px" }}
+                onClick={onClick}
+            >
+                {buttonText}
+            </button>
+        </>
+    );
+});
+
+export default CounterButton;
